01418bc3ed3a4f5dcf88787f1082e64782778ec6
showanne <showanne.e@gmail.com>
2021-07-06 21:46:31 +0800

    0706 update

--- back/routes/users.js
+++ back/routes/users.js
@@ -1,15 +1,22 @@
 import express from 'express'
-// import auth from '../middleware/auth.js'
+import auth from '../middleware/auth.js'
+// 從 controller 引用 function
 import {
   register,
-  login
+  login,
+  logout
 } from '../controllers/users.js'
 
+// 建立 router
 const router = express.Router()
 
+// 此處'/' 路徑為 /users
 // 註冊
 router.post('/', register)
 // 登入
 router.post('/login', login)
+// 登出，登出前驗證
+router.delete('/logout', auth, logout)
 
+// 匯出路由設定
 export default router
--- front/src/store/index.js
+++ front/src/store/index.js
@@ -8,8 +8,11 @@ export default new Vuex.Store({
     // 將後端傳回的資料存 Vuex
     jwt: {
       token: '',
-      // 計算取得 token 的時間
+      // 時間戳記 - 計算取得 token 的時間
+      // 不用每次開網頁都更新序號，等到大概過了5~6天再去換 (後端設定7天過期)，但是每次開網頁都要與後端確認序號可不可以
       received: 0
+      // typeof new Date().getTime() 類型是 number
+      // new Date().getTime() -> 1625498219087
     },
     user: {
       account: '',
@@ -25,18 +28,33 @@ export default new Vuex.Store({
       state.user.account = data.account
       state.user.role = data.role
       state.user.email = data.email
+    },
+    logout (state) {
+      // 登出 將存 Vuex 的資料清空
+      state.jwt = {
+        token: '',
+        received: 0
+      }
+      state.user = {
+        account: '',
+        role: 0,
+        email: ''
+      }
     }
   },
   actions: {
   },
   modules: {
   },
-  // getters 是處理
+  // getters (Vuex) 是先將 Vuex 的資料處理後，再return出來
   getters: {
     user (state) {
-      // mixin 與 getters 的差異
-      // ...state.user (解構寫法) 將上方 state: {user} 展開
-      return { islogin: state.user.account.length > 0, isAdmin: state.user.role === 1, ...state.user }
+      // ...state.user 其餘運算子 (解構寫法) 將上方 state: {user} 展開
+      return {
+        islogin: state.user.account.length > 0,
+        isAdmin: state.user.role === 1,
+        ...state.user
+      }
     }
   }
 })
