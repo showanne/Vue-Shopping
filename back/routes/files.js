453fd232d345d0095c411a56097106473ce7840f
showanne <showanne.e@gmail.com>
2021-07-13 10:34:55 +0800

    upload deploy

--- back/routes/files.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { getFile } from '../controllers/files.js'
-
-const router = express.Router()
-
-// 取得上傳的圖片
-router.get('/:file', getFile)
-
-export default router
